b4662b739117f1ee7e524bbfe3faaa8ece128a89
unknown <olorijoseph2002@gmail.com>
2025-04-29 00:09:01 -0700

    updated files and codes

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//javascript function for signin
+let out=document.getElementById("out");
+out.addEventListener("focus",function(){
+ out.style.backgroundColor="red";
+})
+out.addEventListener("blur",function(){
+ out.style.backgroundColor="#2AAA8A";
+})
+
+function greet(event){
+  let a=document.getElementById("yes").checked;
+   let b=document.getElementById("no").checked;
+   let c=document.getElementById("pu")
+if(a) {
+ alert("login with your details.");
+}
+else{ 
+event.preventDefault();
+c.style.backgroundColor="red";  
+}
+}
+
+
+//javascript function for review
 function review(event){
    let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
@@ -10,19 +34,32 @@ else{
   alert("please fill in the form");
 }
 }
-function greet(event){
-   let a=document.getElementById("yes").checked;
-    let b=document.getElementById("no").checked;
-    let c=document.getElementById("pu")
-if(a) {
-  alert("login with your details.");
-}
-else{ 
-event.preventDefault();
-c.style.backgroundColor="red";  
-}
 
-}
+//javascript function for sign up
+
+let nam=document.getElementById("nam");
+    let ail=document.getElementById("ail");
+     let ph=document.getElementById("ph");
+     nam.addEventListener("focus",function(){
+      nam.style.backgroundColor="red";
+     })
+     nam.addEventListener("blur",function(){
+      nam.style.backgroundColor="white";
+     })
+     ail.addEventListener("focus",function(){
+      ail.style.backgroundColor="red";
+     })
+     ail.addEventListener("blur",function(){
+      ail.style.backgroundColor="white";
+     })
+     ph.addEventListener("focus",function(){
+      ph.style.backgroundColor="red";
+     })
+     ph.addEventListener("blur",function(){
+      ph.style.backgroundColor="white";
+     })
+
+
 function signup(event){
   let nam=document.getElementById("nam").value;
     let mail=document.getElementById("ail").value;
@@ -31,3 +68,10 @@ function signup(event){
        alert("Welcome to Mathematics Guru")
      }
 }
+let pu=document.getElementById("pu");
+pu.addEventListener("focus",function(){
+ pu.style.backgroundColor="red";
+})
+pu.addEventListener("blur",function(){
+ pu.style.backgroundColor="#2AAA8A";
+})
